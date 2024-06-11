b708b7f7b66e1c87d74effdeb30238a17f116f2a
haziq <haziq.zur@gmail.com>
2024-06-11 06:46:19 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Define a route for comments
+app.get('/comments', (req, res) => {
+    // Your logic to fetch and return comments goes here
+    res.send('Comments will be displayed here.');
+});
+
+// Listen on port 8080
+app.listen(8080, () => {
+    console.log('Express server is running at (http://localhost:8080/)');
+});
